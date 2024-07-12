df8b529bc8a1f06361d10a5b57a43d12058527b3
Stas-prog <139768018+Stas-prog@users.noreply.github.com>
2024-07-12 22:35:25 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import { REACT_APP_API_URL } from "../utils/consts";
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || REACT_APP_API_URL
+    baseURL: REACT_APP_API_URL||process.env.REACT_APP_API_URL
 })
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || REACT_APP_API_URL
+    baseURL: REACT_APP_API_URL||process.env.REACT_APP_API_URL
 })
 
 const authInterceptor = config => {
