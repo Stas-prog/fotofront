74f5f0646c8f0f31ad4cf1d858f103f0d09733f2
Stas-prog <139768018+Stas-prog@users.noreply.github.com>
2024-07-16 00:01:31 +0300

    Update FotoModal.js

--- src/components/modals/FotoModal.js
+++ src/components/modals/FotoModal.js
@@ -14,15 +14,15 @@ const FotoModal = observer(({ show, onHide }) => {
             show={show}
             onHide={onHide}
             centered
-        >
+            style={{ background: 'Orchid' }} >
 
-            <Modal.Body             style={{ background: 'CornflowerBlue' }}>
+            <Modal.Body style={{ background: 'CornflowerBlue' }}>
                 <Container className="mt-3">
                     <Image style={{ objectFit: 'contain' }} width={450} height={500} 
     src={REACT_APP_API_URL + foto.selectedFoto.img||process.env.REACT_APP_API_URL + foto.selectedFoto.img} />
                 </Container>
             </Modal.Body>
-            <Modal.Footer             style={{ background: 'CornflowerBlue' }}>
+            <Modal.Footer style={{ background: 'CornflowerBlue' }}>
                 <Button variant="outline-danger" onClick={onHide}>Закрити</Button>
             </Modal.Footer>
         </Modal>
